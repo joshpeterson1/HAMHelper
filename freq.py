d1fe0eb2a8404e16f11348cdc9cbe867ab7174df
somber <joshpeterson6.0@gmail.com>
2023-12-05 15:45:42 -0700

    Tabs update
    intial tabs udpate more work needed

--- freq.py
+++ freq.py
@@ -1,23 +1,28 @@
 import tkinter as tk
+from tkinter import ttk
 
+# Function to calculate wavelength
 def calculate_wavelength(event):
     try:
-        frequency = float(entry.get())
+        frequency = float(freq_entry.get())
         wavelength = 300 / frequency
-        output_label.config(text=str(wavelength))
+        freq_output_label.config(text=str(wavelength))
     except ValueError:
-        output_label.config(text="Invalid input")
+        freq_output_label.config(text="Invalid input")
     except ZeroDivisionError:
-        output_label.config(text="Infinity")
+        freq_output_label.config(text="Infinity")
 
+# Function to toggle always on top
 def toggle_always_on_top():
     root.attributes('-topmost', always_on_top_var.get())
 
+# Function to update opacity
 def update_opacity(value):
     root.attributes('-alpha', float(value))
 
 root = tk.Tk()
-root.title("Frequency to Wavelength Converter")
+root.title("Radio Helper")
+root.iconbitmap('freq.ico')  # Set the window icon
 
 # Set the window size
 screen_width = root.winfo_screenwidth()
@@ -26,21 +31,40 @@ window_width = int(screen_width * 0.1)
 window_height = int(screen_height * 0.15)
 root.geometry(f"{window_width}x{window_height}")
 
-entry = tk.Entry(root)
-entry.pack()
-entry.bind("<KeyRelease>", calculate_wavelength)
+# Create the tab control
+tab_control = ttk.Notebook(root)
 
-output_label = tk.Label(root, text="")
-output_label.pack()
+# Frequency Tab
+freq_tab = ttk.Frame(tab_control)
+tab_control.add(freq_tab, text='Freq')
+freq_entry = tk.Entry(freq_tab)
+freq_entry.pack()
+freq_entry.bind("<KeyRelease>", calculate_wavelength)
+freq_output_label = tk.Label(freq_tab, text="")
+freq_output_label.pack()
 
+# Ohm's Law Tab
+ohms_tab = ttk.Frame(tab_control)
+tab_control.add(ohms_tab, text='Ohms Law')
+# Add Ohm's Law widgets here
+
+# Power Tab
+power_tab = ttk.Frame(tab_control)
+tab_control.add(power_tab, text='Power')
+# Add Power calculation widgets here
+
+# Settings Tab
+settings_tab = ttk.Frame(tab_control)
+tab_control.add(settings_tab, text='Settings')
 # Checkbox for always on top
 always_on_top_var = tk.BooleanVar()
-always_on_top_check = tk.Checkbutton(root, text="Always on top", var=always_on_top_var, command=toggle_always_on_top)
+always_on_top_check = tk.Checkbutton(settings_tab, text="Always on top", var=always_on_top_var, command=toggle_always_on_top)
 always_on_top_check.pack()
-
 # Opacity slider
-opacity_scale = tk.Scale(root, from_=0.1, to=1.0, resolution=0.1, orient=tk.HORIZONTAL, label="Opacity", command=update_opacity)
+opacity_scale = tk.Scale(settings_tab, from_=0.1, to=1.0, resolution=0.1, orient=tk.HORIZONTAL, label="Opacity", command=update_opacity)
 opacity_scale.set(1.0)  # Set default opacity to 100%
 opacity_scale.pack()
+
+tab_control.pack(expand=1, fill="both")
 
 root.mainloop()
