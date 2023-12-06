79cec5e3e8f162410c75ce5bbb740d7bc7f2d60b
somber <joshpeterson6.0@gmail.com>
2023-12-05 21:44:03 -0700

    Renamed Ohms variables
    were conflicting with MP. Need to rename those as well

--- freq.py
+++ freq.py
@@ -80,97 +80,97 @@ def calculate_ohms_law_and_power():
     if not ohms_live_calc_enabled.get():
         return
     try:
-        E = volts_entry.get()
-        I = current_entry.get()
-        R = resistance_entry.get()
-        P = watts_entry.get()
+        ohms_E = ohms_volts_entry.get()
+        ohms_I = ohms_current_entry.get()
+        ohms_R = ohms_resistance_entry.get()
+        ohms_P = ohms_watts_entry.get()
         power_calculated = False
 
         # Reset background colors
-        volts_entry.config(bg='white')
-        current_entry.config(bg='white')
-        resistance_entry.config(bg='white')
-        watts_entry.config(bg='white')
+        ohms_volts_entry.config(bg='white')
+        ohms_current_entry.config(bg='white')
+        ohms_resistance_entry.config(bg='white')
+        ohms_watts_entry.config(bg='white')
 
         # E and I provided
-        if E and I:
-            R = float(E) / float(I)
-            P = float(E) * float(I)
-            resistance_entry.delete(0, tk.END)
-            resistance_entry.insert(0, str(R))
-            watts_entry.delete(0, tk.END)
-            watts_entry.insert(0, str(P))
-            resistance_entry.config(bg='lightgreen')
-            watts_entry.config(bg='lightgreen')
+        if ohms_E and ohms_I:
+            ohms_R = float(ohms_E) / float(ohms_I)
+            ohms_P = float(ohms_E) * float(ohms_I)
+            ohms_resistance_entry.delete(0, tk.END)
+            ohms_resistance_entry.insert(0, str(ohms_R))
+            ohms_watts_entry.delete(0, tk.END)
+            ohms_watts_entry.insert(0, str(ohms_P))
+            ohms_resistance_entry.config(bg='lightgreen')
+            ohms_watts_entry.config(bg='lightgreen')
 
         # E and R provided
-        elif E and R:
-            I = float(E) / float(R)
-            P = (float(E) ** 2) / float(R)
-            current_entry.delete(0, tk.END)
-            current_entry.insert(0, str(I))
-            watts_entry.delete(0, tk.END)
-            watts_entry.insert(0, str(P))
-            current_entry.config(bg='lightgreen')
-            watts_entry.config(bg='lightgreen')
+        elif ohms_E and ohms_R:
+            ohms_I = float(ohms_E) / float(ohms_R)
+            ohms_P = (float(ohms_E) ** 2) / float(ohms_R)
+            ohms_current_entry.delete(0, tk.END)
+            ohms_current_entry.insert(0, str(ohms_I))
+            ohms_watts_entry.delete(0, tk.END)
+            ohms_watts_entry.insert(0, str(ohms_P))
+            ohms_current_entry.config(bg='lightgreen')
+            ohms_watts_entry.config(bg='lightgreen')
 
         # I and R provided
-        elif I and R:
-            E = float(I) * float(R)
-            P = (float(I) ** 2) * float(R)
-            volts_entry.delete(0, tk.END)
-            volts_entry.insert(0, str(E))
-            watts_entry.delete(0, tk.END)
-            watts_entry.insert(0, str(P))
-            volts_entry.config(bg='lightgreen')
-            watts_entry.config(bg='lightgreen')
+        elif ohms_I and ohms_R:
+            ohms_E = float(ohms_I) * float(ohms_R)
+            ohms_P = (float(ohms_I) ** 2) * float(ohms_R)
+            ohms_volts_entry.delete(0, tk.END)
+            ohms_volts_entry.insert(0, str(ohms_E))
+            ohms_watts_entry.delete(0, tk.END)
+            ohms_watts_entry.insert(0, str(ohms_P))
+            ohms_volts_entry.config(bg='lightgreen')
+            ohms_watts_entry.config(bg='lightgreen')
 
         # E and P provided
-        elif E and P:
-            I = float(P) / float(E)
-            R = (float(E) ** 2) / float(P)
-            current_entry.delete(0, tk.END)
-            current_entry.insert(0, str(I))
-            resistance_entry.delete(0, tk.END)
-            resistance_entry.insert(0, str(R))
-            current_entry.config(bg='lightgreen')
-            resistance_entry.config(bg='lightgreen')
+        elif ohms_E and ohms_P:
+            ohms_I = float(ohms_P) / float(ohms_E)
+            ohms_R = (float(ohms_E) ** 2) / float(ohms_P)
+            ohms_current_entry.delete(0, tk.END)
+            ohms_current_entry.insert(0, str(ohms_I))
+            ohms_resistance_entry.delete(0, tk.END)
+            ohms_resistance_entry.insert(0, str(ohms_R))
+            ohms_current_entry.config(bg='lightgreen')
+            ohms_resistance_entry.config(bg='lightgreen')
 
         # I and P provided
-        elif I and P:
-            E = float(P) / float(I)
-            R = float(P) / (float(I) ** 2)
-            volts_entry.delete(0, tk.END)
-            volts_entry.insert(0, str(E))
-            resistance_entry.delete(0, tk.END)
-            resistance_entry.insert(0, str(R))
-            volts_entry.config(bg='lightgreen')
-            resistance_entry.config(bg='lightgreen')
+        elif ohms_I and ohms_P:
+            ohms_E = float(ohms_P) / float(ohms_I)
+            ohms_R = float(ohms_P) / (float(ohms_I) ** 2)
+            ohms_volts_entry.delete(0, tk.END)
+            ohms_volts_entry.insert(0, str(ohms_E))
+            ohms_resistance_entry.delete(0, tk.END)
+            ohms_resistance_entry.insert(0, str(ohms_R))
+            ohms_volts_entry.config(bg='lightgreen')
+            ohms_resistance_entry.config(bg='lightgreen')
 
         # R and P provided
-        elif R and P:
-            E = (float(P) * float(R)) ** 0.5
-            I = (float(P) / float(R)) ** 0.5
-            volts_entry.delete(0, tk.END)
-            volts_entry.insert(0, str(E))
-            current_entry.delete(0, tk.END)
-            current_entry.insert(0, str(I))
-            volts_entry.config(bg='lightgreen')
-            current_entry.config(bg='lightgreen')
+        elif ohms_R and ohms_P:
+            ohms_E = (float(ohms_P) * float(ohms_R)) ** 0.5
+            ohms_I = (float(ohms_P) / float(ohms_R)) ** 0.5
+            ohms_volts_entry.delete(0, tk.END)
+            ohms_volts_entry.insert(0, str(ohms_E))
+            ohms_current_entry.delete(0, tk.END)
+            ohms_current_entry.insert(0, str(ohms_I))
+            ohms_volts_entry.config(bg='lightgreen')
+            ohms_current_entry.config(bg='lightgreen')
 
     except ValueError:
         # Invalid input, do nothing
         pass
 
 def reset_ohms_law():
-    volts_entry.delete(0, tk.END)
-    current_entry.delete(0, tk.END)
-    resistance_entry.delete(0, tk.END)
-    watts_entry.delete(0, tk.END)
-    volts_entry.config(bg='white')
-    current_entry.config(bg='white')
-    resistance_entry.config(bg='white')
-    watts_entry.config(bg='white')
+    ohms_volts_entry.delete(0, tk.END)
+    ohms_current_entry.delete(0, tk.END)
+    ohms_resistance_entry.delete(0, tk.END)
+    ohms_watts_entry.delete(0, tk.END)
+    ohms_volts_entry.config(bg='white')
+    ohms_current_entry.config(bg='white')
+    ohms_resistance_entry.config(bg='white')
+    ohms_watts_entry.config(bg='white')
 
 def calculate_power_parameters():
     global calculation_id  # Declare calculation_id as global
@@ -311,34 +311,34 @@ tab_control.add(ohms_tab, text='Ohms')
 # Voltage (E)
 volts_label = tk.Label(ohms_tab, text="E (Volts):")
 volts_label.pack(side=tk.TOP, anchor='w', padx=10)
-volts_entry = tk.Entry(ohms_tab)
-volts_entry.pack(side=tk.TOP, anchor='w', padx=10)
-volts_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_volts_entry = tk.Entry(ohms_tab)
+ohms_volts_entry.pack(side=tk.TOP, anchor='w', padx=10)
+ohms_volts_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
 
 # Current (I)
 current_label = tk.Label(ohms_tab, text="I (Current):")
 current_label.pack(side=tk.TOP, anchor='w', padx=10)
-current_entry = tk.Entry(ohms_tab)
-current_entry.pack(side=tk.TOP, anchor='w', padx=10)
-current_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_current_entry = tk.Entry(ohms_tab)
+ohms_current_entry.pack(side=tk.TOP, anchor='w', padx=10)
+ohms_current_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
 
 # Resistance (R)
 resistance_label = tk.Label(ohms_tab, text="R (Resistance):")
 resistance_label.pack(side=tk.TOP, anchor='w', padx=10)
-resistance_entry = tk.Entry(ohms_tab)
-resistance_entry.pack(side=tk.TOP, anchor='w', padx=10)
-resistance_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_resistance_entry = tk.Entry(ohms_tab)
+ohms_resistance_entry.pack(side=tk.TOP, anchor='w', padx=10)
+ohms_resistance_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
 
-volts_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
-current_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
-resistance_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_volts_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_current_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_resistance_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
 
 # Watts (P)
 watts_label = tk.Label(ohms_tab, text="P (Watts):")
 watts_label.pack(side=tk.TOP, anchor='w', padx=10)
-watts_entry = tk.Entry(ohms_tab)
-watts_entry.pack(side=tk.TOP, anchor='w', padx=10)
-watts_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
+ohms_watts_entry = tk.Entry(ohms_tab)
+ohms_watts_entry.pack(side=tk.TOP, anchor='w', padx=10)
+ohms_watts_entry.bind("<KeyRelease>", lambda event: calculate_ohms_law_and_power())
 
 # Reset button (centered)
 reset_button = tk.Button(ohms_tab, text="Reset", command=reset_ohms_law)
